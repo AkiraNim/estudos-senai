d991d1968d47ba6e0efeaefd7a31b86e3872fab7
Akira <152340630+AkiraNim@users.noreply.github.com.>
2025-02-12 19:26:21 -0300

    lunch

--- Lunch/src/lanche/Main.java
+++ Lunch/src/lanche/Main.java
@@ -18,62 +18,62 @@ public class Main {
         boolean sair = false;
         List<Lanche> lanche = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        
+
         while (!sair) {
             System.out.println("Bem vindo ao sistema de lanches PICA - Polo Industrial Cães Asmáticos"
                     + "\n[1]- Cadastrar um lanche."
                     + "\n[2] - Vender um lanche."
                     + "\n[3] - Sair.");
-            
+
             escolha = obterEntradaInteira(scanner);
-            
+
             switch (escolha) {
                 case 1:
                     System.out.println("\nVocê escolheu cadastrar um lanche.");
                     System.out.println("\nDigite o nome do lanche.");
                     String nome = scanner.next();
-                    
+
                     System.out.println("\nDigite o preço do lanche.");
                     float preco = obterEntradaFloat(scanner);
-                    
+
                     System.out.println("\nDigite o caminho da imagem do lanche.");
                     String imagem = scanner.next();
-                    imagem = imagem.replace("\"", ""); 
+                    imagem = imagem.replace("\"", "");
                     imagem = imagem.replace("\\", "/");
-                   
+
                     Path caminhoImagem = copiarArquivos(Paths.get(imagem), Paths.get("Imagens"));
-                    
+
                     if(caminhoImagem == null) {
-                    	System.out.println("Erro ao copiar a imagem. Cadastro cancelado.");
+                        System.out.println("Erro ao copiar a imagem. Cadastro cancelado.");
                         break;
                     }
-                    
+
                     Lanche lanche2 = new Lanche(codigo, nome, preco, caminhoImagem.toString());
                     lanche.add(lanche2);
-                    
+
                     System.out.println("\nLanche " + nome + ", codigo "+ codigo +" cadastrado com sucesso.");
                     codigo++;
                     break;
-                
+
                 case 2:
                     System.out.println("\nVocê escolheu vender um lanche.");
                     if (!lanche.isEmpty()) {
                         System.out.println("\nDigite o código do lanche.");
                         int codigo2 = obterEntradaInteira(scanner);
-                        
+
                         for (Lanche lanche3 : lanche) {
                             if (lanche3.getCodigo() == codigo2) {
                                 System.out.println("\nO lanche " + lanche3.getEspecificacao() + " custa R$"
                                         + lanche3.getPreco() + ".\nQuantos gostaria de comprar?");
                                 int quantidade = obterEntradaInteira(scanner);
-                                
+
                                 int opcao = 0;
                                 while (opcao != 1 && opcao != 2) {
                                     System.out.println("O total de " + quantidade + " " + lanche3.getEspecificacao() + " é " + (quantidade * lanche3.getPreco())
                                             + "\nEscolha uma opção:"
                                             + "\n[1] - Comprar."
                                             + "\n[2] - Sair.");
-                                    
+
                                     opcao = obterEntradaInteira(scanner);
                                     if (opcao == 1) {
                                         System.out.println("Lanche comprado.");
@@ -91,12 +91,12 @@ public class Main {
                         System.out.println("\nNenhum lanche foi cadastrado.");
                     }
                     break;
-                
+
                 case 3:
                     System.out.println("\nVocê escolheu sair.");
                     sair = true;
                     break;
-                
+
                 default:
                     System.out.println("Opção inválida.");
                     break;
@@ -104,7 +104,7 @@ public class Main {
         }
         scanner.close();
     }
-    
+
     public static int obterEntradaInteira(Scanner scanner) {
         while (true) {
             try {
@@ -115,7 +115,7 @@ public class Main {
             }
         }
     }
-    
+
     public static float obterEntradaFloat(Scanner scanner) {
         while (true) {
             try {
@@ -126,18 +126,18 @@ public class Main {
             }
         }
     }
-    
+
     public static Path copiarArquivos(Path origem, Path pastaDestino) {
-    	try {
-    		if(!Files.exists(pastaDestino)) {
-    			Files.createDirectories(pastaDestino);
-    		}
-    		Path destino = pastaDestino.resolve(origem.getFileName());
-    		Files.copy(origem, destino, StandardCopyOption.REPLACE_EXISTING);
-    		return destino;
-    	}catch(IOException e){
-    		System.out.println("\nErro ao copiar a imagem: " + e.getMessage());
+        try {
+            if(!Files.exists(pastaDestino)) {
+                Files.createDirectories(pastaDestino);
+            }
+            Path destino = pastaDestino.resolve(origem.getFileName());
+            Files.copy(origem, destino, StandardCopyOption.REPLACE_EXISTING);
+            return destino;
+        }catch(IOException e){
+            System.out.println("\nErro ao copiar a imagem: " + e.getMessage());
             return null;
-    	}
+        }
     }
 }
