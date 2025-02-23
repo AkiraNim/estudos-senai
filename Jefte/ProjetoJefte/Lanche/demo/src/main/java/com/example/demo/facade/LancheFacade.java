c65ca685f59ab58f8817636895c5a82cf077fb2e
Akira Ni <152340630+AkiraNim@users.noreply.github.com>
2025-02-23 05:27:54 -0300

    Atividade 3

---
+++ Jefte/ProjetoJefte/Lanche/demo/src/main/java/com/example/demo/facade/LancheFacade.java
@@ -0,0 +1,39 @@
+package com.example.demo.facade;
+
+import com.example.demo.applications.LancheApplication;
+import com.example.demo.entities.Lanche;
+import com.example.demo.repositories.LancheRepository;
+import com.example.demo.services.LancheService;
+
+import java.util.List;
+
+public class LancheFacade {
+    private LancheApplication lancheApplication;
+
+    public LancheFacade(LancheApplication lancheApplication) {
+        this.lancheApplication = lancheApplication;
+    }
+
+    public void cadastrar(Lanche lanche) {
+        this.lancheApplication.cadastrar(lanche);
+    }
+
+    public List<Lanche> buscar() {
+        return this.lancheApplication.buscar();
+    }
+
+    public Lanche buscarPorCodigo(int codigo) {
+        return this.lancheApplication.buscarPorCodigo(codigo);
+    }
+
+    public double calcularLanche(Lanche lanche, int quantidade) {
+        return this.lancheApplication.calcularLanche(lanche, quantidade);
+    }
+
+    public void remover(int codigo) {
+        lancheApplication.remover(codigo);
+    }
+    public void atualizar(int codigo, Lanche lanche) {
+        lancheApplication.atualizar(codigo, lanche);
+    }
+}
