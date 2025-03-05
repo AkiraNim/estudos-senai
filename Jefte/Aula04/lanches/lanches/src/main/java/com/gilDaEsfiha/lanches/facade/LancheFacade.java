c586d4989feef2c1c55046ace5054df56d06bdb0
Akira Ni <152340630+AkiraNim@users.noreply.github.com>
2025-03-05 20:14:31 -0300

    update atividade jefte

---
+++ Jefte/Aula04/lanches/lanches/src/main/java/com/gilDaEsfiha/lanches/facade/LancheFacade.java
@@ -0,0 +1,40 @@
+package com.gilDaEsfiha.lanches.facade;
+
+
+
+import com.gilDaEsfiha.lanches.controller.LancheApplication;
+import com.gilDaEsfiha.lanches.domain.Lanche;
+
+import java.util.List;
+
+public class LancheFacade {
+    private LancheApplication lancheApplication;
+
+    public LancheFacade(LancheApplication lancheApplication) {
+        this.lancheApplication = lancheApplication;
+    }
+
+    public void cadastrar(Lanche lanche, String destino) {
+        this.lancheApplication.cadastrar(lanche, destino);
+    }
+
+    public List<Lanche> buscar() {
+        return this.lancheApplication.buscar();
+    }
+
+    public Lanche buscarPorCodigo(int codigo) {
+        return this.lancheApplication.buscarPorCodigo(codigo);
+    }
+
+    public double calcularLanche(Lanche lanche, int quantidade) {
+        return this.lancheApplication.calcularLanche(lanche, quantidade);
+    }
+
+    public void remover(int codigo, Lanche lanche, String destino) {
+        lancheApplication.remover(codigo, lanche, destino);
+    }
+    public void atualizar(int codigo, Lanche lanche, String destino) {
+        lancheApplication.atualizar(codigo, lanche, destino);
+
+    }
+}
