db0a445346c09fb4ca6c5cb3668d5ee174479eec
Akira Ni <152340630+AkiraNim@users.noreply.github.com>
2025-03-30 21:10:20 -0300

    update javascript

---
+++ Terceiro Semestre/Estudo/Javascript/Aula02/js/script.js
@@ -0,0 +1,9 @@
+let a,b,c;
+
+a=5;
+b=6;
+c=7;
+
+function minhaFuncao(){
+    return null;
+}
---
+++ Terceiro Semestre/Estudo/Javascript/Aula03/js/script.js
@@ -0,0 +1,13 @@
+var a = 2;
+var b = 3;
+var c = a+b;
+
+
+
+let x = 10;
+
+{
+    let y = 20;
+    
+}
+document.getElementById("texto").innerHTML = x;
