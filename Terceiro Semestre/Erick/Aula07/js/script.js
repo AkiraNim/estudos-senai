29c2e3659979662685a1192c5cefd53a275a45ee
Akiranim <152340630+AkiraNim@users.noreply.github.com>
2025-04-03 21:19:59 -0300

    update erick

---
+++ Terceiro Semestre/Erick/Aula07/js/script.js
@@ -0,0 +1,5 @@
+const multiplicationForm = document.querySelector("#multiplication-form");
+const numerInput = document.querySelector("#number");
+const multiplicationInput = document.querySelector("#multiplicator");
+
+const multiplicationTitle = document.querySelector("#multiplication-title span")
