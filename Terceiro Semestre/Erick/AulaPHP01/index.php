f88a97ba4561db6868c0b37149c69c34be7b05ac
Akiranim <152340630+AkiraNim@users.noreply.github.com>
2025-04-11 21:05:11 -0300

    update

---
+++ Terceiro Semestre/Erick/AulaPHP01/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula01 - PHP</title>
+</head>
+<body>
+    <h1>Minha primeira pagina PHP</h1> 
+    <h2><?php echo "Hello World!"; echo 2+2;?></h2>
+</body>
+</html>
